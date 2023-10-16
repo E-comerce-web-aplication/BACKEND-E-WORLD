25a7ca7ab7b381abdfa124490489c2c16acb3098
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-10-16 10:58:31 -0600

    Creacion y modificacion de estructura del proyecto

---
+++ inventory.ArqLimpia.EN/Company.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.ArqLimpia.EN
+{
+    public class Company
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRequired]
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonRequired]
+        [BsonElement("Description")]
+        public string Description { get; set; }
+
+        [BsonRequired]
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonRequired]
+        [BsonElement("Address")]
+        public string Address { get; set; }
+
+    }
+}
