36ea4d5c574e8c70ecc318d533475cc779eb07f0
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-10-16 21:55:58 -0600

    Creacion de CompanyBL

--- Inventory.ArqLimpia.BL/CompanyBL.cs
+++ Inventory.ArqLimpia.BL/CompanyBL.cs
@@ -1,5 +1,8 @@
-﻿using Inventory.ArqLimpia.BL.DTOs;
+﻿using Inventary.ArqLimpia.DAL;
+using inventory.ArqLimpia.EN;
+using Inventory.ArqLimpia.BL.DTOs;
 using Inventory.ArqLimpia.BL.Interfaces.Interfaces;
+using Inventory.ArqLimpia.EN.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +12,88 @@ using System.Threading.Tasks;
 namespace Inventory.ArqLimpia.BL
 {
     public class CompanyBL : ICompanyBL
-
-
     {
-        public Task<CreateCompanyOutputDTO> CreateCompany(CreateCompanyInputDTO cCompany)
+        readonly ICompany _companyDAL;
+
+        public CompanyBL(ICompany companyDAL)
         {
-            throw new NotImplementedException();
+            _companyDAL = companyDAL;
         }
 
-        public Task<List<FindOneCompanyOutputDTOs>> Find(FindCompanyOutputDTOs cCompany)
+        public async Task<CreateCompanyOutputDTO> CreateCompany(CreateCompanyInputDTO cCompany)
         {
-            throw new NotImplementedException();
+            var newCompany = new Company()
+            {
+                Name = cCompany.Name,
+                Description = cCompany.Description,
+                Email = cCompany.Email,
+                Address = cCompany.Address,
+
+            };
+
+            var existingCompany = await _companyDAL.FindByName(newCompany.Name);
+            if (existingCompany != null)
+            {
+                throw new ArgumentException("Ya existe una compania con este nombre.");
+            }
+
+            await _companyDAL.Create(newCompany);
+
+            var companiesOutput = new CreateCompanyOutputDTO()
+            {
+                Id = newCompany.Id,
+                Name = newCompany.Name,
+                Description = newCompany.Description,
+                Email = newCompany.Email,
+                Address = newCompany.Address
+            };
+
+            return companiesOutput;
         }
 
-        public Task<FindOneCompanyOutputDTOs> FindOne(FindByIdDTO cCompany)
+        public async Task<List<FindOneCompanyOutputDTOs>> Find(FindCompanyOutputDTOs cCompany)
         {
-            throw new NotImplementedException();
+            var company = await _companyDAL.Find(new Company
+            {
+                Id = cCompany.Id,
+                Name = cCompany.Name,
+                Description = cCompany.Description,
+                Email = cCompany.Email,
+                Address = cCompany.Address
+
+            });
+
+            var resultList = new List<FindOneCompanyOutputDTOs>();
+            company.ForEach(product => resultList.Add(new FindOneCompanyOutputDTOs
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Email = product.Email,
+                Address = product.Address
+            }));
+
+            return resultList;
+        }
+
+
+        public async Task<FindOneCompanyOutputDTOs> FindOne(FindByIdDTO cCompany)
+        {
+            var company = await _companyDAL.FindOne(cCompany.Id);
+            if (company != null)
+            {
+                var companies = new FindOneCompanyOutputDTOs
+                {
+                    Id = company.Id,
+                    Name = company.Name,
+                    Description = company.Description,
+                    Email = company.Email,
+                    Address = company.Address
+                };
+                return companies;
+            }
+
+            throw new Exception($"Company con ID {cCompany.Id} no encontrado");
         }
     }
 }
