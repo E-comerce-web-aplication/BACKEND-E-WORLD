ec22de4103c11d5b230815d3fa8c50b753238359
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-10-23 13:38:41 -0600

    Creacion de IStore

---
+++ Inventory.EN.Enterprice/IStore.cs
@@ -0,0 +1,17 @@
+﻿using inventory.ArqLimpia.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ArqLimpia.EN.Interfaces
+{
+    public interface IStore
+    {
+        Task Create(StoreEN sStore);
+        Task<StoreEN> FindOne(string Id);
+        Task<List<StoreEN>> Find(StoreEN store);
+        Task<StoreEN> FindByName(string Name);
+    }
+}
