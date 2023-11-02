5fda7c31828b54e7b8ed06b9a39d7fffae03b29c
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-11-02 17:00:20 -0600

    Creaacion de ProductHistoryDTOs

---
+++ Inventory.ArqLimpia.BL.DTOs/ProductHistoryDTOs.cs
@@ -0,0 +1,60 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Inventory.ArqLimpia.BL.DTOs
+{
+    public class ProductHistoryDTOs
+    {
+        public class DeleteHistoryInputDTOs
+        {
+            [Required]
+            public string IdProduct { get; set; }
+        }
+
+        public class DeleteHistoryOutputDTOs
+        {
+            public bool IsDeleted { get; set; }
+        }
+
+        public class FindOneHistoryInputDTOs
+        {
+            [Required(ErrorMessage = "You must enter a title for the search")]
+            public string ProductName { get; set; }
+        }
+
+        public class FindByIdDTO
+        {
+            [Required(ErrorMessage = "You must enter a id for the search")]
+            public string Id { get; set; }
+        }
+
+        public class FindOneHistoryOutputDTOs
+        {
+            public string Id { get; set; }
+            public string ProductName { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public List<string> Images { get; set; }
+            public int Stock { get; set; }
+            public double Price { get; set; }
+            public string CompanyId { get; set; }
+            public string SendConditions { get; set; }
+            public List<string> Tags { get; set; }
+        }
+
+        public class FindHistoryOutputDTOs
+        {
+            public string Id { get; set; }
+            public string ProductName { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public List<string> Images { get; set; }
+            public int Stock { get; set; }
+            public double Price { get; set; }
+            public string CompanyId { get; set; }
+            public string SendConditions { get; set; }
+            public List<string> Tags { get; set; }
+        }
+    }
+}
--- Inventory.ArqLimpia.BL.Interfaces/Interfaces/IProductHistory.cs
+++ Inventory.ArqLimpia.BL.Interfaces/Interfaces/IProductHistory.cs
@@ -1,12 +1,12 @@
 ﻿using Inventory.ArqLimpia.BL.DTOs;
-
+using static Inventory.ArqLimpia.BL.DTOs.ProductHistoryDTOs;
 
 namespace Inventory.ArqLimpia.BL.Interfaces.Interfaces
 {
     public interface IProductHistory
     {
-        Task<DeleteHistoryOutputDTOs> Delete(DeleteHistoryInputDTOs pProducts);
-        Task<List<FindOneHistoryOutputDTOs>> Find(FindHistoryOutputDTOs pProducts);
-        Task<FindOneHystoryOutputDTOs> FindOne(FindByIdDTOs pProducts);
+        Task<DeleteHistoryOutputDTOs> Delete(DeleteHistoryInputDTOs pHistory);
+        Task<List<FindOneHistoryOutputDTOs>> Find(FindHistoryOutputDTOs pHistory);
+        Task<FindOneHistoryOutputDTOs> FindOne(FindByIdDTO pHistory);
     }
 }
