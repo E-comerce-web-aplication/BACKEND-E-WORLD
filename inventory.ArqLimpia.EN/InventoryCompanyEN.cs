caf0fb5eaf256a8fad1113dfdb3f6b55ca7553bb
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-11-19 09:54:47 -0600

    Creacion de InventoryCompanyEN

---
+++ inventory.ArqLimpia.EN/InventoryCompanyEN.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.ArqLimpia.EN
+{
+    public class InventoryCompanyEN
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("CompanyId")]
+        public int CompanyId { get; set; }
+
+        [BsonElement("Company")]
+        public string Company { get; set; }
+
+        [BsonElement("ProductId")]
+        public ObjectId ProductId { get; set; }
+
+        [BsonElement("Quantity")]
+        public int Quantity { get; set; }
+    }
+}
