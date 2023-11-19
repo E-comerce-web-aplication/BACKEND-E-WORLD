15d5094cddfccf6b7d340e1948c5f56bd50dfbf6
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-11-19 09:53:40 -0600

    Creacion de InventoryStoreEN

---
+++ inventory.ArqLimpia.EN/InventoryStoreEN.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.ArqLimpia.EN
+{
+    public class InventoryStoreEN
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("StoreId")]
+        public int StoreId { get; set; }
+
+        [BsonElement("ProductId")]
+        public ObjectId ProductId { get; set; }
+
+        [BsonElement("Quantity")]
+        public int Quantity { get; set; }
+    }
+}
