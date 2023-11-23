391d3ec0339c299acf05eadb39e4bddb55acf51f
Nathy0209 <nathalia202301@outlook.com>
2023-11-23 08:43:47 -0600

    crear Interface

--- Inventory.EN.Enterprice/IProvider.cs
+++ Inventory.EN.Enterprice/IProvider.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using inventory.ArqLimpia.EN;
 
 namespace Inventory.ArqLimpia.EN.Interfaces
 {
-    internal interface IProvider
+   public interface IProvider
     {
+        Task Create(ProviderEN pProvider);
+        Task Update(ProviderEN pProvider);
+        Task Delete(string pProvider);
+        Task<ProviderEN> FindOne(string providerId);
+        Task<List<ProviderEN>> Find(ProviderEN provider);
+        Task<ProviderEN> FindByName(string providerName);
     }
 }
